ebf182cf5acd01dfa3265805ca4e20f5560ae31c
Pankaj Singh <pkstiyara@gmail.com>
2022-10-19 17:24:37 +0530

    changing the user data for apache2

--- webserver.tf
+++ webserver.tf
@@ -175,11 +175,9 @@ resource "aws_instance" "web-server-instance" {
 
     user_data = <<-EOF
                 #!bin/bash
-                sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "The page was created by the user data" | sudo tee /var/www/html/index.html
+                sudo apt-get update && sudo apt-get upgrade -y
+                sudo apt install apache2 -y
+                sudo systemctl enable apache2
                 EOF
     
     tags = {
