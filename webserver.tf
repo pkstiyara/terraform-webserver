75635d9ae94b29cdffeaa7be73fe5eab8b2204a4
pkstiyara <pkstiyara@gmail.com>
2023-01-14 17:41:47 +0530

    run2

--- webserver.tf
+++ webserver.tf
@@ -1,9 +1,6 @@
-provider "aws" {
-    region = "us-east-1"
-    # access_key = "" DO not use
-    # secret_key = "" Do not use
-  
-}
+# provider "aws" {
+#     region = "us-east-1"
+# }
 
 
 # resource "aws_instance" "my-server" {
